b70776db7c5e148b9c82ca3f149905865e1380b7
plevytskyi <alivelp@gmail.com>
2016-05-25 11:50:44 +0300

    fix hostname

--- src/run.py
+++ src/run.py
@@ -4,5 +4,6 @@ from app import app, db
 
 db.create_all()
 port = os.getenv('PORT', 5000)
-print(port)
+hostname = os.getenv('URL', 'hostname')
+print('port={}, hostname={}'.format(port, hostname))
 app.run(debug=True, port=port)
